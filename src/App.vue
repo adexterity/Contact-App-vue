d39d1ea19dbb75534d53d7c98407b317ec0ec07a
Joseph Suberu <opensesame4me@gmail.com>
2023-05-26 20:20:55 +0100

    initialcommit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Home from "./components/Home.vue";
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- content container -->
+  <div class="container">
+    <nav>
+      <ul>
+        <li>home icon</li>
+        <li>contact icon</li>
+        <li>message icon</li>
+        <li>settings icon</li>
+      </ul>
+    </nav>
+    <!-- main section -->
+    <main>
+      <div></div>
+      <div></div>
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style scoped></style>
